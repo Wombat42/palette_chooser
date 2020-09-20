ee51d8e1ac997d2588a0ba7dbc001c5385d3f37c
Andy Wilson <andy@savvyduck.com>
2020-09-20 10:44:03 -0400

    added random color picker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
+import { rgb } from "polished";
 import "./styles.css";
 import { Card } from "./components/colorcard";
 import { ColorForm } from "./components/colorform";
 
+function getRandomInt(max = 255) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function randomColor() {
+  return rgb(getRandomInt(), getRandomInt(), getRandomInt(),)
+}
+
 export default function App() {
+const startColor = useMemo(randomColor, []);
   const [colors, setColors] = useState([]);
   return (
     <div className="App">
       <ColorForm
-        color={"#2A1D94"}
+        color={startColor}
         colorsChange={(newColors) => {
           setColors(newColors);
         }}
